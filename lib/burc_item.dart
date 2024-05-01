c8266b886b0bcbcb4c75d660d3c11fcce2eacd43
Mstf466 <mstf466@outlook.com>
2024-05-01 11:53:23 +0300

    flutter_list_error

--- lib/burc_item.dart
+++ lib/burc_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_burclar/burc_detay.dart';
 import 'package:flutter_burclar/model/burc.dart';
 
 class BurcItem extends StatelessWidget {
@@ -7,15 +8,32 @@ class BurcItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var myTextStyle = Theme.of(context).textTheme;
     return Card(
+      elevation: 4,
       child: ListTile(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => BurcDetay(secilenBurc: listelenenBurc),
+            ),
+          );
+        },
         leading: Image.asset(
           "images/" + listelenenBurc.burcKucukResim,
-          width: 64,
-          height: 64,
         ),
-        title: Text(listelenenBurc.burcAdi),
-        subtitle: Text(listelenenBurc.burcTarihi),
+        title: Text(
+          listelenenBurc.burcAdi,
+          style: myTextStyle.headline6,
+        ),
+        subtitle: Text(
+          listelenenBurc.burcTarihi,
+          style: myTextStyle.subtitle2,
+        ),
+        trailing: Icon(
+          Icons.arrow_forward_ios,
+          color: Colors.pink,
+        ),
       ),
     );
   }
